cc52c8bafa300347f1b6c4a68761494c3a4d6047
Marlon Santos <marlon-20-12@hotmail.com>
2022-12-12 16:52:08 -0300

    Login

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class loginPage extends StatelessWidget {
-  const loginPage({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
